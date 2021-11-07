21c0596ec26d002f2c94dd5cb7cde0ecf14dc6b1
naoki <65482672+naokiweb@users.noreply.github.com>
2021-11-07 21:41:57 +0900

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -137,28 +137,15 @@ jQuery(function ($) {
       bodyFixReset();
     });
 
-
-  // // ======================================================
-  // // footer__top__btn
-  // // ======================================================
-  {
-    $('.footer__top__btn').click(function() {
-        $('html, body').animate({
-            'scrollTop': 0
-        }, 1000);
-    });
-  } 
-
-  $(function(){
-    $('a[href^=#]').click(function() {
-    var speed = 500;
-    var href= $(this).attr("href");
-    var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top;
-    $('body,html').animate({scrollTop:position}, speed, 'swing');
-    return false;
-    });
-    });
-    
-
+    $(function() {
+      // スムーススクロール
+      $('a[href^="#"]').click(function(){
+          var speed = 1000;
+          var href= $(this).attr("href");
+          var target = $(href == "#" || href == "" ? 'html' : href);
+          var position = target.offset().top;
+          $("html, body").animate({scrollTop:position}, speed, "swing");
+          return false;
+      });
+  });
 });
